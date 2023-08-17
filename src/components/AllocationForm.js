75c601da21271158cffbe53bcb0f2159be227ed3
Colin-Cargile <141424944+Colin-Cargile@users.noreply.github.com>
2023-08-17 16:38:05 -0500

    Update AllocationForm.js

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -9,12 +9,17 @@ const AllocationForm = (props) => {
     const [action, setAction] = useState('');
 
     const submitEvent = () => {
-
-            if(cost > remaining) {
-                alert("The value cannot exceed remaining funds  £"+remaining);
-                setCost("");
-                return;
-            }
+        
+        if(cost > 20000) {
+            alert("The value cannot exceed 20000");
+            setCost("");
+            return;
+        }
+        if(cost > remaining) {
+            alert("The value cannot exceed remaining funds  £"+remaining);
+            setCost("");
+            return;
+        }
 
         const expense = {
             name: name,
@@ -36,44 +41,43 @@ const AllocationForm = (props) => {
     return (
         <div>
             <div className='row'>
-
-            <div className="input-group mb-3" style={{ marginLeft: '2rem' }}>
+                <div className="input-group mb-3" style={{ marginLeft: '2rem' }}>
                     <div className="input-group-prepend">
-                <label className="input-group-text" htmlFor="inputGroupSelect01">Department</label>
-                  </div>
-                  <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setName(event.target.value)}>
+                        <label className="input-group-text" htmlFor="inputGroupSelect01">Department</label>
+                    </div>
+                    <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setName(event.target.value)}>
                         <option defaultValue>Choose...</option>
                         <option value="Marketing" name="marketing"> Marketing</option>
-                <option value="Sales" name="sales">Sales</option>
-                <option value="Finance" name="finance">Finance</option>
-                <option value="HR" name="hr">HR</option>
-                <option value="IT" name="it">IT</option>
-                <option value="Admin" name="admin">Admin</option>
-                  </select>
+                        <option value="Sales" name="sales">Sales</option>
+                        <option value="Finance" name="finance">Finance</option>
+                        <option value="HR" name="hr">HR</option>
+                        <option value="IT" name="it">IT</option>
+                        <option value="Admin" name="admin">Admin</option>
+                    </select>
 
                     <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
-                <label className="input-group-text" htmlFor="inputGroupSelect02">Allocation</label>
-                  </div>
-                  <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
+                        <label className="input-group-text" htmlFor="inputGroupSelect02">Allocation</label>
+                    </div>
+                    <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
                         <option defaultValue value="Add" name="Add">Add</option>
-                <option value="Reduce" name="Reduce">Reduce</option>
-                  </select>
-
+                        <option value="Reduce" name="Reduce">Reduce</option>
+                    </select>
+                    <label for="cost" style={{ marginLeft: '2rem' , size: 10}}>£</label>
                     <input
                         required='required'
                         type='number'
+                        step="10"
+                        max="20000"
                         id='cost'
                         value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
-                        </input>
+                    </input>
 
                     <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
                         Save
                     </button>
                 </div>
-                </div>
-
+            </div>
         </div>
     );
 };
